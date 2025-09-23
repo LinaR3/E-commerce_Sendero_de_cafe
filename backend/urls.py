baab18f9f803d7eb716571d0af36c34f99c70589
Lina Reina <linareinap3@gmail.com>
2025-09-23 05:24:27 +0000

    HTML

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from product.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),          
-    path('products/', include('product.urls')),  
+    path('', include('product.urls')),  
 ]
